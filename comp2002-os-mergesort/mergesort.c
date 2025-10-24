ac27d87c9a9c0f767c77cdb587661ed6421bf4ff
rlew7887 <rachelchienwei.lew@student.adelaide.edu.au>
2025-10-25 01:35:12 +1030

    Implement merge function

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -9,6 +9,42 @@
 
 /* this function will be called by mergesort() and also by parallel_mergesort(). */
 void merge(int leftstart, int leftend, int rightstart, int rightend){
+	int leftsize = leftend - leftstart + 1;
+	int rightsize = rightend - rightstart;
+
+	for (int i=0; i<leftsize; i++) 
+		B[i] = A[leftstart + i]; /*left subarray*/
+	for (int j=0; j<rightsize; j++)
+		B[leftsize + j] = A[rightstart + j]; /*right subarray*/
+
+	/*merge temp array B back into original array A*/
+	int i = 0;
+	int j = 0;
+	int k = leftstart;
+	while (i<leftsize && j<rightsize) {
+		if (B[i] <= B[leftsize + j]) {
+			A[k] = B[i];
+			i++;
+		} else {
+			A[k] = B[leftsize + j];
+			j++;
+		}
+		k++;
+	}
+
+	/*copy remaining elements of left subarray, if any*/
+	while (i < leftsize) {
+		A[k] = B[i];
+		i++;
+		k++;
+	}
+
+	/*copy remaining elements of right subarray, if any*/
+	while (j < rightsize) {
+		A[k] = B[leftsize + j];
+		j++;
+		k++;
+	}
 }
 
 /* this function will be called by parallel_mergesort() as its base case. */
