8d8cf1dece5cbc26f85abc1dd3a9e6e2051509f2
inkfeeder <100576433+inkfeeder@users.noreply.github.com>
2025-10-25 14:23:27 +1030

    my_mergesort implementation

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -49,7 +49,16 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 
 /* this function will be called by parallel_mergesort() as its base case. */
 void my_mergesort(int left, int right){
+
+	if (left >= right) return;	/*if array has 1 or 0 elements, its already sorted*/
+
+		int mid = (left + right) / 2; /*find midpoint*/
+		my_mergesort(left, mid);
+		my_mergesort(mid + 1, right);
+		merge(left, mid, mid + 1, right); /*merge two sorted halves*/
+
 }
+
 
 /* this function will be called by the testing program. */
 void * parallel_mergesort(void *arg){
